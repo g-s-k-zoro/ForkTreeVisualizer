18861a33cb3d53a143d597367490747d6e261c3e
Ghanashyam Katti <ghassamghatti1@gmail.com>
2021-04-04 22:40:34 +0530

    Tree generated, Need to add custom code injection

--- forkTreeBuilder.cpp
+++ forkTreeBuilder.cpp
@@ -8,8 +8,35 @@ using namespace std;
 
 #define main_parent 9
 const string FILENAME = "graph.txt";
-void generateTreeImage(map<long, vector<long> >nodeMappings) {
 
+void generateTreeImage(map<long, vector<long> >nodeMapping) {
+
+	fstream dotFile;
+	dotFile.open("dotfile.gv", ios::trunc | ios::out);
+
+	int count = 0;
+
+	dotFile<<"digraph processTree {\n";
+	for(auto ppid = nodeMapping.begin(); ppid != nodeMapping.end(); ppid++) {
+		if( ppid->first == main_parent ){
+			dotFile<<"MAIN_PROCESS->"<<ppid->second[0]<<";\n";
+			count++;
+		}
+		else {
+			for(auto pid = ppid->second.begin(); pid != ppid->second.end(); pid++) {
+				dotFile<<ppid->first<<"->"<<*pid<<";\n";
+				count++;
+			}
+		}
+	}
+	dotFile<<"Processes_Spawned_"<<count<<" [shape=polygon, sides=4, peripheries=2, color = red];\n";
+	dotFile<<"MAIN_PROCESS [shape=polygon,sides=6,peripheries=2,color=lightgreen,style=filled]\n}";
+
+	dotFile.close();
+
+	//Generrating the PNG file
+	string command = "dot -Tpng dotfile.gv -o ForkTree.png";
+	system(command.c_str());
 }
 
 void generateTree() {
@@ -21,28 +48,23 @@ void generateTree() {
 	long parent, child;
 	int index;
 	
-	while(file) {
+	while(!file.eof()) {
 		
 		getline(file, input);
+		
+		if(file.eof())
+			break;
+		
 		index = input.find(":");
 		stringstream P(input.substr(0, index));
 		stringstream C(input.substr(index+1, input.size()-index-1) );
-		
 		P>>parent; C>>child;
 		nodeMapping[ parent ].push_back( child );
-		cout<<"Child : "<<child<<endl;
 	}
 
 	file.close();
 
-	for(auto ppid = nodeMapping.begin(); ppid != nodeMapping.end(); ppid++) {
-
-		cout<<ppid->first<<" : ";
-		for(auto  pid = ppid->second.begin(); pid != ppid->second.end(); pid++) {
-			cout<<*pid<<", ";
-		}
-		cout<<endl;
-	}
+	generateTreeImage(nodeMapping);
 }
 
 
@@ -52,13 +74,14 @@ int main(int argc, char const *argv[])
 	fstream file;
 	file.open(FILENAME, ios::trunc | ios::out | ios::in);
 
+	//Custom Code Begins
 	fork();
 	fork() && fork() || fork();
 	fork();
 
-	sleep(2);
+	//Custom Code Ends
 	file<<(long)getppid()<<":"<<(long)getpid()<<endl;
-	cout<<"1"<<endl;
+
 	sleep(2);
 
 	if((long)getppid() != main_parent)
